36a8c686de08afdac0d12bf1542c0c50aaf979fb
Anthony Jordan <anthonyjordan@Anthonys-MacBook-Pro.local>
2023-02-07 10:41:10 -0500

    bozo

---
+++ untitled/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+
+
+int main()
+{
+   cout << "What's your favorite word??? " << endl;
+
+     string x;
+
+   cin >> x;
+
+   int y = x.length();
+   int n;
+
+   for (int n = 0; y < n; n++)
+   cout << "*";
+
+   cout << "****" << y << endl;
+   cout << "* " << x << " *" << endl;
+   cout << "****" << y << endl;
+
+}
+
